5ccd310326c745016033f5aa9c9c517424b24378
Kyumin Kim <math4tots@gmail.com>
2015-12-13 01:08:32 -0800

    first commit

---
+++ word.py
@@ -0,0 +1,22 @@
+poss = [
+  'yseobr',
+  'kagwie',
+  'sgzykm',
+]
+
+words = [
+  'about', 'after', 'again', 'below', 'could',
+  'every', 'first', 'found', 'great', 'house',
+  'large', 'learn', 'never', 'other', 'place',
+  'plant', 'point', 'right', 'small', 'sound',
+  'spell', 'still', 'study', 'their', 'there',
+  'these', 'thing', 'think', 'three', 'water',
+  'where', 'which', 'world', 'would', 'write',
+]
+
+copy = set(words)
+
+for i, p in enumerate(poss):
+  copy = set(w for w in copy if w[i] in p)
+
+print(copy)
